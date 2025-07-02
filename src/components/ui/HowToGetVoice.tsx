adf9ad99555d0dc4951bb782a3ceaa59997f3e93
kingfish8km <kingfish8km@gmail.com>
2025-07-02 14:43:51 +0900

    受け取り方の流れのモバイル版を作成

--- src/components/ui/HowToGetVoice.tsx
+++ src/components/ui/HowToGetVoice.tsx
@@ -15,105 +15,105 @@ export default function HowToGetVoice() {
 
     if (displayValue === 'small') {
         return (
-            <Stack bg="#FFFFFF"  py={40} align="center"  w="100vw" >
-                <Text textStyle="4xl" marginBottom={20} color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlowMobile")}</Text>
-                <Flex align="center" gap="0px" py={8} justify="space-between" w="90vw">
-                    <Flex direction="column" align="flex-start" w="50vw">
-                        <Text textStyle="4xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow1")}</Text>
-                        <Box mt={4} px={12}>
-                            <Text textStyle="xl" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow1detail1")}</Text>
+            <Stack bg="#FFFFFF"  py={20} align="center"  w="100vw" >
+                <Text textStyle="2xl"  color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlowMobile")}</Text>
+                <Flex direction="column" align="center" gap="0px" py={8} justify="space-between" w="92vw">
+                    <Flex direction="column" align="flex-start" w="92vw">
+                        <Text textStyle="2xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow1")}</Text>
+                        <Box mt={4} px={2}>
+                            <Text textStyle="lg" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow1detail1")}</Text>
                         </Box>
                     </Flex>
-                    <Flex gap="8px">
-                        <Image src={Rainbow} alt="Voice Recording" width={240} height={520} />
+                    <Flex gap="8px" mt={4}>
+                        <Image src={Rainbow} alt="Voice Recording" width={360} height={780} />
                     </Flex>
                 </Flex>
-                <Flex align="center" gap="0px" py={8} justify="space-between" w="90vw">
-                    <Flex direction="column" align="flex-start" w="50vw">
-                        <Text textStyle="4xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow2")}</Text>
-                        <Box mt={4} px={12}>
-                            <Text textStyle="xl" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow2detail1")}</Text>
+                <Flex direction="column" align="center" gap="0px" py={8} justify="space-between" w="92vw">
+                    <Flex direction="column" align="flex-start" w="92vw">
+                        <Text textStyle="2xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow2")}</Text>
+                        <Box mt={4} px={2}>
+                            <Text textStyle="lg" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow2detail1")}</Text>
                         </Box>
                     </Flex>
-                    <Flex gap="8px">
-                        <Image src={Rainbow} alt="Voice Recording" width={240} height={520} />
+                    <Flex gap="8px" mt={4}>
+                        <Image src={Rainbow} alt="Voice Recording" width={360} height={780} />
                     </Flex>
                 </Flex>
-                <Flex align="center" gap="0px" py={8} justify="space-between" w="90vw">
-                    <Flex direction="column" align="flex-start" w="50vw">
-                        <Text textStyle="4xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow3")}</Text>
-                        <Box mt={4} px={12}>
-                            <Text textStyle="xl" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow3detail1")}</Text>
+                <Flex direction="column" align="center" gap="0px" py={8} justify="space-between" w="92vw">
+                    <Flex direction="column" align="flex-start" w="92vw">
+                        <Text textStyle="2xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow3")}</Text>
+                        <Box mt={4} px={2}>
+                            <Text textStyle="lg" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow3detail1")}</Text>
                         </Box>
                     </Flex>
-                    <Flex gap="8px">
-                        <Image src={Rainbow} alt="Voice Recording" width={240} height={520} />
+                    <Flex gap="8px" mt={4}>
+                        <Image src={Rainbow} alt="Voice Recording" width={360} height={780} />
                     </Flex>
                 </Flex>
-                <Flex align="center" gap="0px" py={8} justify="space-between" w="90vw">
-                    <Flex direction="column" align="flex-start" w="50vw">
-                        <Text textStyle="4xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow4")}</Text>
-                        <Box mt={4} px={12}>
-                            <Text textStyle="xl" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow4detail1")}</Text>
+                <Flex direction="column" align="center" gap="0px" py={8} justify="space-between" w="92vw">
+                    <Flex direction="column" align="flex-start" w="92vw">
+                        <Text textStyle="2xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow4")}</Text>
+                        <Box mt={4} px={2}>
+                            <Text textStyle="lg" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow4detail1")}</Text>
                         </Box>
                     </Flex>
-                    <Flex gap="8px">
-                        <Image src={Rainbow} alt="Voice Recording" width={240} height={520} />
+                    <Flex gap="8px" mt={4}>
+                        <Image src={Rainbow} alt="Voice Recording" width={360} height={780} />
                     </Flex>
                 </Flex>
-                <Flex align="center" gap="0px" py={8} justify="space-between" w="90vw">
-                    <Flex direction="column" align="flex-start" w="50vw">
-                        <Text textStyle="4xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow5")}</Text>
-                        <Box mt={4} px={12}>
-                            <Text textStyle="xl" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow5detail1")}</Text>
+                <Flex direction="column" align="center" gap="0px" py={8} justify="space-between" w="92vw">
+                    <Flex direction="column" align="flex-start" w="92vw">
+                        <Text textStyle="2xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow5Mobile")}</Text>
+                        <Box mt={4} px={2}>
+                            <Text textStyle="lg" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow5detail1")}</Text>
                         </Box>
                     </Flex>
-                    <Flex gap="8px">
-                        <Image src={Rainbow} alt="Voice Recording" width={240} height={520} />
+                    <Flex gap="8px" mt={4}>
+                        <Image src={Rainbow} alt="Voice Recording" width={360} height={780} />
                     </Flex>
                 </Flex>
-                <Flex align="center" gap="0px" py={8} justify="space-between" w="90vw">
-                    <Flex direction="column" align="flex-start" w="50vw">
-                        <Text textStyle="4xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow6")}</Text>
-                        <Box mt={4} px={12}>
-                            <Text textStyle="xl" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow6detail1")}</Text>
+                <Flex direction="column" align="center" gap="0px" py={8} justify="space-between" w="92vw">
+                    <Flex direction="column" align="flex-start" w="92vw">
+                        <Text textStyle="2xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow6")}</Text>
+                        <Box mt={4} px={2}>
+                            <Text textStyle="lg" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow6detail1")}</Text>
                         </Box>
                     </Flex>
-                    <Flex gap="8px">
-                        <Image src={Rainbow} alt="Voice Recording" width={240} height={520} />
+                    <Flex gap="8px" mt={4}>
+                        <Image src={Rainbow} alt="Voice Recording" width={360} height={780} />
                     </Flex>
                 </Flex>
-                <Flex align="center" gap="0px" py={8} justify="space-between" w="90vw">
-                    <Flex direction="column" align="flex-start" w="50vw">
-                        <Text textStyle="4xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow7")}</Text>
-                        <Box mt={4} px={12}>
-                            <Text textStyle="xl" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow7detail1")}</Text>
+                <Flex direction="column" align="center" gap="0px" py={8} justify="space-between" w="92vw">
+                    <Flex direction="column" align="flex-start" w="92vw">
+                        <Text textStyle="2xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow7Mobile")}</Text>
+                        <Box mt={4} px={2}>
+                            <Text textStyle="lg" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow7detail1")}</Text>
                         </Box>
                     </Flex>
-                    <Flex gap="8px">
-                        <Image src={Rainbow} alt="Voice Recording" width={240} height={520} />
+                    <Flex gap="8px" mt={4}>
+                        <Image src={Rainbow} alt="Voice Recording" width={360} height={780} />
                     </Flex>
                 </Flex>
-                <Flex align="center" gap="0px" py={8} justify="space-between" w="90vw">
-                    <Flex direction="column" align="flex-start" w="50vw">
-                        <Text textStyle="4xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow8")}</Text>
-                        <Box mt={4} px={12}>
-                            <Text textStyle="xl" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow8detail1")}</Text>
+                <Flex direction="column" align="center" gap="0px" py={8} justify="space-between" w="92vw">
+                    <Flex direction="column" align="flex-start" w="92vw">
+                        <Text textStyle="2xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow8")}</Text>
+                        <Box mt={4} px={2}>
+                            <Text textStyle="lg" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow8detail1")}</Text>
                         </Box>
                     </Flex>
-                    <Flex gap="8px">
-                        <Image src={Rainbow} alt="Voice Recording" width={240} height={520} />
+                    <Flex gap="8px" mt={4}>
+                        <Image src={Rainbow} alt="Voice Recording" width={360} height={780} />
                     </Flex>
                 </Flex>
-                <Flex align="center" gap="0px" py={8} justify="space-between" w="90vw">
-                    <Flex direction="column" align="flex-start" w="50vw">
-                        <Text textStyle="4xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow9")}</Text>
-                        <Box mt={4} px={12}>
-                            <Text textStyle="xl" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow9detail1")}</Text>
+                <Flex direction="column" align="center" gap="0px" py={8} justify="space-between" w="92vw">
+                    <Flex direction="column" align="flex-start" w="92vw">
+                        <Text textStyle="2xl" color="#C85062" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow9")}</Text>
+                        <Box mt={4} px={2}>
+                            <Text textStyle="lg" color="#6D7685" textAlign="left" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("AcceptFlow9detail1")}</Text>
                         </Box>
                     </Flex>
-                    <Flex gap="8px">
-                        <Image src={Rainbow} alt="Voice Recording" width={240} height={520} />
+                    <Flex gap="8px" mt={4}>
+                        <Image src={Rainbow} alt="Voice Recording" width={360} height={780} />
                     </Flex>
                 </Flex>
                 <PageMoveButtonLong toRef="/security" buttonLabel={t("CheckSecurity")} />
