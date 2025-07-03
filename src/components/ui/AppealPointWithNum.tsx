04457005f4a91d1d19e03d82ebc4ce2c7592a842
kingfish8km <kingfish8km@gmail.com>
2025-07-03 22:40:35 +0900

    モバイルの〇の番号を白に

--- src/components/ui/AppealPointWithNum.tsx
+++ src/components/ui/AppealPointWithNum.tsx
@@ -21,21 +21,21 @@ export default function AppealPointWithNum(props: AppealPointWithNumProps) {
     if (displayValue === 'small') {
         return (
             <Flex direction="column" px={4} py={2}>
-                <Box borderRadius={20} bg="#C85062" w={10} h={10} fontSize={20} padding={1}  zIndex={1} >{props.num}</Box>
+                <Box borderRadius={20} bg="#C85062" color="#FFFFFF" w={10} h={10} fontSize={20} padding={1}  zIndex={1} >{props.num}</Box>
                 <AppealPoint pointNum={props.num} appealSentence={props.appealSentence} detailSentence={props.detailSentence} wpoint={props.wpoint ? props.wpoint : 0} hpoint={props.hpoint ? props.hpoint : 0} />
             </Flex>
         );
     } else if (displayValue === 'medium') {
         return (
             <Flex direction="column" px={4} py={4}>
-                <Box borderRadius="25px" bg="#C85062" w="50px" h="50px" fontSize={30} textAlign="center" justifyContent="center" paddingRight="1px" paddingTop="1px" zIndex={1} >{props.num}</Box>
+                <Box borderRadius="25px" bg="#C85062" color="#FFFFFF" w="50px" h="50px" fontSize={30} textAlign="center" justifyContent="center" paddingRight="1px" paddingTop="1px" zIndex={1} >{props.num}</Box>
                 <AppealPoint pointNum={props.num} appealSentence={props.appealSentence} detailSentence={props.detailSentence} wpoint={480} hpoint={props.hpoint ? props.hpoint : 0} />
             </Flex>
         );
     } else {
         return (
             <Flex direction="column" px={4} py={4}>
-                <Box borderRadius="25px" bg="#C85062" w="50px" h="50px" fontSize={30} textAlign="center" justifyContent="center" paddingRight="1px" paddingTop="1px" zIndex={1} >{props.num}</Box>
+                <Box borderRadius="25px" bg="#C85062" color="#FFFFFF" w="50px" h="50px" fontSize={30} textAlign="center" justifyContent="center" paddingRight="1px" paddingTop="1px" zIndex={1} >{props.num}</Box>
                 <AppealPoint pointNum={props.num} appealSentence={props.appealSentence} detailSentence={props.detailSentence} wpoint={480} hpoint={428} />
             </Flex>
         );
