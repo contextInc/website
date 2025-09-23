d652b5d23cc2a08c3a3dbe524e4080b8e7b86468
kingfish8km <kingfish8km@gmail.com>
2025-09-23 14:09:44 +0900

    モバイルのボイスギフトチケットの表示について修正

--- src/components/ui/VoiceTicketInfo.tsx
+++ src/components/ui/VoiceTicketInfo.tsx
@@ -18,20 +18,14 @@ export default function VoiceTicketInfo() {
         return (
             <>
                 <Stack bg="#FFFFFF" py={30} style={{transform: "translateZ(0)"}}  alignItems="center" w="100vw" >
-                    <Text textStyle="2xl" marginBottom={10} color="#C85062" whiteSpace="pre-wrap"  fontFamily='Open Sans, sans-serif'>{t("VoiceTicket")}</Text>
+                    <Text textStyle="2xl" marginBottom={10} color="#C85062" whiteSpace="pre-wrap"  fontFamily='Open Sans, sans-serif'>{t("VoiceGiftTicket")}</Text>
                     <Separator color="#000000" width="85vw" height={2} marginBottom={4} />
-                    <Box w="85vw" >
+                    <Box w="85vw">
                         <VoiceTicketExplanation content={t("VoiceTicketExplanation")} />
                         <VoiceTicketPriceItem ticketNum={1} ticketDetail={t("OneVoiceTicketPrice")} />
-                        <VoiceTicketPriceItem ticketNum={2} ticketDetail={t("TwoVoiceTicketPrice")} />
-                        <VoiceTicketPriceItem ticketNum={3} ticketDetail={t("ThreeVoiceTicketPrice")} />
-                        <VoiceTicketPriceItem ticketNum={4} ticketDetail={t("FourVoiceTicketPrice")} />
-                        <VoiceTicketPriceItem ticketNum={6} ticketDetail={t("SixVoiceTicketPrice")} />
-                        <VoiceTicketPriceItem ticketNum={8} ticketDetail={t("EightVoiceTicketPrice")} />
-                        <VoiceTicketPriceItem ticketNum={10} ticketDetail={t("TenVoiceTicketPrice")} />
-                        <Box h="80px" w="85vw" marginBottom="20px">
+                        {/* <Box h="80px" w="85vw" marginBottom="20px">
                             <Text textStyle="lg" textAlign="left" color="#6D7685" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("SubscriptionTicketExplanation")}</Text>
-                        </Box>
+                        </Box> */}
                         {/* <SubscriptionNotice title={t("BusinessDescription")} content={t("BusinessDescriptionDetail")} /> */}
                     </Box>
                     <Separator color="#000000" width="85vw" height={2} marginBottom={4} />
@@ -42,20 +36,14 @@ export default function VoiceTicketInfo() {
         return (
             <>
                 <Stack bg="#FFFFFF" py={30} style={{transform: "translateZ(0)"}}  alignItems="center" w="100vw" >
-                    <Text textStyle="5xl" marginBottom={10} color="#C85062" whiteSpace="pre-wrap"  fontFamily='Open Sans, sans-serif'>{t("VoiceTicket")}</Text>
+                    <Text textStyle="5xl" marginBottom={10} color="#C85062" whiteSpace="pre-wrap"  fontFamily='Open Sans, sans-serif'>{t("VoiceGiftTicket")}</Text>
                     <Separator color="#000000" width="85vw" height={2} marginBottom={4} />
                     <Box w="80vw" >
                         <VoiceTicketExplanation content={t("VoiceTicketExplanation")} />
                         <VoiceTicketPriceItem ticketNum={1} ticketDetail={t("OneVoiceTicketPrice")} />
-                        <VoiceTicketPriceItem ticketNum={2} ticketDetail={t("TwoVoiceTicketPrice")} />
-                        <VoiceTicketPriceItem ticketNum={3} ticketDetail={t("ThreeVoiceTicketPrice")} />
-                        <VoiceTicketPriceItem ticketNum={4} ticketDetail={t("FourVoiceTicketPrice")} />
-                        <VoiceTicketPriceItem ticketNum={6} ticketDetail={t("SixVoiceTicketPrice")} />
-                        <VoiceTicketPriceItem ticketNum={8} ticketDetail={t("EightVoiceTicketPrice")} />
-                        <VoiceTicketPriceItem ticketNum={10} ticketDetail={t("TenVoiceTicketPrice")} />
-                        <Box h="80px" w="80vw">
+                        {/* <Box h="80px" w="80vw">
                             <Text textStyle="2xl" textAlign="left" color="#6D7685" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{t("SubscriptionTicketExplanation")}</Text>
-                        </Box>
+                        </Box> */}
                         {/* <SubscriptionNotice title={t("BusinessDescription")} content={t("BusinessDescriptionDetail")} /> */}
                     </Box>
                     <Separator color="#000000" width="85vw" height={2} marginBottom={4} />
--- src/components/ui/VoiceTicketPriceItem.tsx
+++ src/components/ui/VoiceTicketPriceItem.tsx
@@ -20,7 +20,7 @@ export default function VoiceTicketPriceItem(props: SubscriptionPriceItemProps) 
         return (
             <Box h="60px" w="85vw">
                 <Flex gap={8} textAlign="left">
-                    <Text textStyle="lg" color="#6D7685" w="160px" fontWeight="bold" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{props.ticketNum + t("Piece")}</Text>
+                    <Text textStyle="lg" color="#6D7685" w="160px" fontWeight="bold" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{props.ticketNum + t("PiecePerVoiceGiftTicket")}</Text>
                     <Text textStyle="lg" color="#6D7685" w="160px" fontWeight="bold" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{props.ticketDetail}</Text>
                     {/* <Text textStyle="lg" color="#6D7685" w="200px" fontWeight="bold" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{props.pricePerTicket}</Text> */}
                     {/* <Text textStyle="lg" color="#6D7685" fontWeight="bold" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{props.pricePerMonth}</Text> */}
@@ -31,7 +31,7 @@ export default function VoiceTicketPriceItem(props: SubscriptionPriceItemProps) 
         return (
             <Box h={100} w="85vw">
                 <Flex gap={8} textAlign="left">
-                    <Text textStyle="2xl" color="#6D7685" w="300px" fontWeight="bold" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{props.ticketNum + t("Piece")}</Text>
+                    <Text textStyle="2xl" color="#6D7685" w="300px" fontWeight="bold" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{props.ticketNum + t("PiecePerVoiceGiftTicket")}</Text>
                     <Text textStyle="2xl" color="#6D7685" w="200px" fontWeight="bold" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{props.ticketDetail}</Text>
                     {/* <Text textStyle="2xl" color="#6D7685" fontWeight="bold" whiteSpace="pre-wrap" fontFamily='Open Sans, sans-serif'>{props.pricePerTicket}</Text> */}
                 </Flex>
